bcfad97c783bffa1e491b555b96fd86e25dffa75
Eli Bendersky <eliben@gmail.com>
2020-07-02 06:40:19 -0700

    Encoding MVIs with immediates / register refs

--- assembler.js
+++ assembler.js
@@ -70,10 +70,15 @@ class Assembler {
   _encodeInstruction(sl, curAddr) {
     console.log('assembling', JSON.stringify(sl));
     switch (sl.instr.toLowerCase()) {
+      case 'mvi':
+        this._expectArgsCount(sl, 2);
+        let r = this._argR(sl, sl.args[0]);
+        let imm = this._argImm(sl, sl.args[1]);
+        return [0b110 | (r << 3), imm];
       case 'push':
-        let rp = this._expectOneRegisterPairArg(sl);
-        let enc = 0b11000101 | (rp << 4);
-        return [enc];
+        this._expectArgsCount(sl, 1);
+        let rp = this._argRP(sl, sl.args[0]);
+        return [0b11000101 | (rp << 4)];
         break;
       default:
         this._assemblyError(sl.pos, `unknown instruction ${sl.instr}`);
@@ -81,24 +86,77 @@ class Assembler {
     return 1;
   }
 
-  // Expect sl to have a single register pair argument and returns its encoding.
-  _expectOneRegisterPairArg(sl) {
-    if (sl.args.length != 1) {
-      this._assemblyError(sl.pos, `want one RP arg for ${sl.instr}; got ${sl.args.length}`);
+  _expectArgsCount(sl, count) {
+    if (sl.args.length != count) {
+      this._assemblyError(sl.pos, `want ${count} args for ${sl.instr}; got ${sl.args.length}`);
     }
-    let arg = sl.args[0];
+  }
+
+  // Expect arg to be a register pair and return its encoding.
+  _argRP(sl, arg) {
     switch (arg.toLowerCase()) {
-      case 'bc':
-        return 0b00;
-      case 'de':
-        return 0b01;
-      case 'hl':
-        return 0b10;
+      case 'bc': return 0b00;
+      case 'de': return 0b01;
+      case 'hl': return 0b10;
       case 'sp':
       case 'psw':
         return 0b11;
       default:
         this._assemblyError(sl.pos, `invalid register pair ${arg}`);
+    }
+  }
+
+  // Expect arg to be a register and return its encoding.
+  _argR(sl, arg) {
+    switch (arg.toLowerCase()) {
+      case 'a': return 0b111;
+      case 'b': return 0b000;
+      case 'c': return 0b001;
+      case 'd': return 0b010;
+      case 'e': return 0b011;
+      case 'h': return 0b100;
+      case 'l': return 0b101;
+      default:
+        this._assemblyError(sl.pos, `invalid register ${arg}`);
+    }
+  }
+
+  // Expect arg to be an immediate number and return its encoding.
+  _argImm(sl, arg) {
+    let n = this._parseNumber(arg);
+    if (isNaN(n)) {
+      this._assemblyError(sl.post, `invalid immediate ${arg}`);
+    }
+    return n;
+  }
+
+  // Parses numbers in accepted asm format (24, 1Ah, 0101b)
+  // A strict parser of binary, hex and decimal numbers. Returns NaN for invalid
+  // numbers with bad prefixes, etc.
+  _parseNumber(n) {
+    n = n.toLowerCase();
+
+    // Find supported base; validator will be set to a regexp to validate the
+    // number's contents. n will be trimmed if needed.
+    let validator = null;
+    let base = 10;
+
+    if (n.endsWith('h')) {
+      validator = /^[0-9a-f]+$/;
+      n = n.slice(0, -1);
+      base = 16;
+    } else if (n.endsWith('b')) {
+      validator = /^[0-1]+$/;
+      n = n.slice(0, -1);
+      base = 2;
+    } else {
+      validator = /^[0-9]+$/;
+    }
+
+    if (!validator.test(n)) {
+      return NaN;
+    } else {
+      return parseInt(n, base);
     }
   }
 
