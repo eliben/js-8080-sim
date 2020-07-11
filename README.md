8bc1fc509163351fbb56dece87e3cb4e5763ddcd
Eli Bendersky <eliben@gmail.com>
2020-07-11 07:20:15 -0700

    First steps in UI:
    * created textarea for code
    * load/save state with local storage

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 We use `browserify`; see `Makefile` for the invocation.
 
+The files `index.html`, `ui.js` and `js8080simBundle.js` should be distributed
+together to make it all work in the browser.
--- ui.js
+++ ui.js
@@ -1,6 +1,31 @@
+'use strict';
+
+const STORAGE_ID = 'js8080sim';
+
+
+loadState();
+
+document.querySelector("#run").addEventListener("mousedown", runCode);
+
 // Expects the global js8080sim to be available.
 let asm = new js8080sim.Assembler();
 
 let st = document.querySelector("#status");
 st.textContent = "SUCCESS";
 st.style.color = "green";
+
+function loadState() {
+  let state = JSON.parse(localStorage.getItem(STORAGE_ID));
+  if (state) {
+    document.querySelector("#codetext").value = state['codetext'];
+  }
+}
+
+function saveState() {
+  let state = {'codetext': document.querySelector('#codetext').value};
+  localStorage.setItem(STORAGE_ID, JSON.stringify(state));
+}
+
+function runCode() {
+  saveState();
+}
