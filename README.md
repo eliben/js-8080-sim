9a160098e07885aaca14b3d179dfc60bfc4a3d8d
Eli Bendersky <eliben@gmail.com>
2020-07-11 06:52:36 -0700

    Basic ui.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # js-8080-sim
+
+## Bundling for the browser
+
+We use `browserify`; see `Makefile` for the invocation.
+
---
+++ ui.js
@@ -0,0 +1,6 @@
+// Expects the global js8080sim to be available.
+let asm = new js8080sim.Assembler();
+
+let st = document.querySelector("#status");
+st.textContent = "SUCCESS";
+st.style.color = "green";
