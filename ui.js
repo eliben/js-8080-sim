9384ff29cd90d500e77b2164f47395a82ab6ec2d
wizche <sergio.paganoni@gmail.com>
2022-10-17 18:49:37 +0200

    (re)added auto indent on enter

--- ui.js
+++ ui.js
@@ -11,7 +11,7 @@ const codetext = document.querySelector('#codetext');
 const maxsteps = document.querySelector('#maxsteps');
 const ramstart = document.querySelector('#ramstart');
 const ramshowmode = document.querySelector('#ramshowmode');
-//codetext.addEventListener('keydown', onCodeTextKey);
+codetext.addEventListener('keydown', onCodeTextKey);
 document.querySelector("#run").addEventListener("mousedown", () => dispatchStep("run"));
 document.querySelector("#prev").addEventListener("mousedown", () => dispatchStep("prev"));
 document.querySelector("#next").addEventListener("mousedown", () => dispatchStep("next"));
@@ -324,7 +324,7 @@ function onPrevStep() {
 function highlightCurrentLine() {
   let pc = parseInt(cpuStateValues.pc.textContent, 16);
   let lineno = globAddrToLine[pc];
-  console.log(`Highlighting ${pc} => ${lineno}`);
+  //console.log(`Highlighting ${pc} => ${lineno}`);
   let lines = codetext.innerHTML.split('\n');
 
   // map the entries to a div to enable styling and track the index if the line contains FAIL
@@ -351,7 +351,6 @@ function onRunCode() {
   let [state, mem, labelToAddr, addrToLine] = runProg(prog, parseInt(maxsteps.value));
   memFromLastRun = mem;
   globAddrToLine = addrToLine;
-  console.log(globAddrToLine);
   // Populate CPU state / registers.
   for (let regName of Object.keys(state)) {
     if (cpuStateValues.hasOwnProperty(regName)) {
@@ -396,11 +395,81 @@ function onRamStartKey(event) {
   }
 }
 
+// Credits to https://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container/4812022#4812022
+function getCaretCharacterOffsetWithin(element) {
+  var caretOffset = 0;
+  var doc = element.ownerDocument || element.document;
+  var win = doc.defaultView || doc.parentWindow;
+  var sel;
+  if (typeof win.getSelection != "undefined") {
+    sel = win.getSelection();
+    if (sel.rangeCount > 0) {
+      var range = win.getSelection().getRangeAt(0);
+      var preCaretRange = range.cloneRange();
+      preCaretRange.selectNodeContents(element);
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      caretOffset = preCaretRange.toString().length;
+    }
+  } else if ( (sel = doc.selection) && sel.type != "Control") {
+    var textRange = sel.createRange();
+    var preCaretTextRange = doc.body.createTextRange();
+    preCaretTextRange.moveToElementText(element);
+    preCaretTextRange.setEndPoint("EndToEnd", textRange);
+    caretOffset = preCaretTextRange.text.length;
+  }
+  return caretOffset;
+}
+
+// Credits to https://stackoverflow.com/questions/6249095/how-to-set-the-caret-cursor-position-in-a-contenteditable-element-div
+function createRange(node, chars, range) {
+  if (!range) {
+    range = document.createRange();
+    range.selectNode(node);
+    range.setStart(node, 0);
+  }
+
+  if (chars.count === 0) {
+    range.setEnd(node, chars.count);
+  } else if (node && chars.count >0) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      if (node.textContent.length < chars.count) {
+        chars.count -= node.textContent.length;
+      } else {
+        range.setEnd(node, chars.count);
+        chars.count = 0;
+      }
+    } else {
+      for (var lp = 0; lp < node.childNodes.length; lp++) {
+        range = createRange(node.childNodes[lp], chars, range);
+
+        if (chars.count === 0) {
+          break;
+        }
+      }
+    }
+  }
+
+  return range;
+}
+
+function setCurrentCursorPosition(element, chars) {
+  if (chars >= 0) {
+    var selection = window.getSelection();
+
+    var range = createRange(element, { count: chars });
+
+    if (range) {
+      range.collapse(false);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+}
 
 function onCodeTextKey(event) {
   if (event.keyCode == 13) {
     // Capture "Enter" to insert spaces similar to the previous line.
-    let pos = codetext.selectionStart;
+    let pos = getCaretCharacterOffsetWithin(codetext);
 
     let prevNewlinePos = pos - 1;
     while (prevNewlinePos > 0 &&
@@ -419,8 +488,7 @@ function onCodeTextKey(event) {
                       '\n' +
                       ' '.repeat(numSpaces) +
                       codetext.innerHTML.substring(pos, codetext.innerHTML.length);
-    codetext.selectionStart = pos + numSpaces + 1;
-    codetext.selectionEnd = pos + numSpaces + 1;
+    setCurrentCursorPosition(codetext, pos + numSpaces + 1);
     event.stopPropagation();
     event.preventDefault();
   }
